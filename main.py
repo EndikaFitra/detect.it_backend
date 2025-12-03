e03b90d41b6df2cb680699473aeb16fe7bbb9909
EndikaFitra <endikfitra10@gmail.com>
2025-12-03 11:37:13 +0700

    commit

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = FastAPI(title="Detect.IT API", description="API untuk mendeteksi kesegaran
 # Konfigurasi CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://detect-it-three.vercel.app/"],
+    allow_origins=["http://localhost:3000", "https://detect-it-three.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
